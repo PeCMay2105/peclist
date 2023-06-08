dbf508555ff61d06d0c92fde1161e3af6a94d372
PeCMay2105 <pedrocmaia2105@gmail.com>
2023-06-08 09:51:01 -0300

    peclist em react

---
+++ src/componentes/menu/index.js
@@ -0,0 +1,18 @@
+import './menu.css'
+
+const Menu = ()=>{
+
+    return(
+        <div className="caixaMae">
+            <div className="caixaMenu">
+                <h3>Todas</h3>
+                <hr></hr>
+                <h3>Concluidas</h3>
+                <hr></hr>
+                <h3>A fazer</h3>
+            </div>
+        </div>
+    )
+}
+
+export default Menu;
